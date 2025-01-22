f10ccfa26492c0df08f29001ea79c71366e5e6c8
rist3vskaj <jristevska@edu.hse.ru>
2025-01-22 16:20:03 +0100

    modified font, textfield color and label repoistion and resizing

--- WhatToDoPlanner/StartScreens/SignUpScreen/SignUpViewController.swift
+++ WhatToDoPlanner/StartScreens/SignUpScreen/SignUpViewController.swift
@@ -26,21 +26,23 @@ final class SignUpViewController: UIViewController {
         static let fontName: String = "AoboshiOne-Regular"
         
         static let titleLabelText: String = "Register"
-        static let fontTitleSize: CGFloat = 34
+        static let fontTitleSize: CGFloat = 50
         static let fieldPlaceholderFontSize: CGFloat = 17
         
         // TextField and Button Dimensions
-        static let textFieldHeight: CGFloat = 50
-        static let textFieldCornerRadius: CGFloat = 8
-        static let buttonHeight: CGFloat = 50
+        static let textFieldHeight: CGFloat = 70
+        static let textFieldCornerRadius: CGFloat = 14
+        static let buttonHeight: CGFloat = 70
         static let buttonWidth: CGFloat = 50
-        static let buttonCornerRadius: CGFloat = 8
-        static let fieldSpacing: CGFloat = 20
+        static let buttonCornerRadius: CGFloat = 14
+        static let fieldSpacing: CGFloat = 100
         static let sidePadding: CGFloat = 20
+        static let textFieldColor: UIColor = .white
         
         // Button Colors
         static let buttonBackgroundColor: UIColor = UIColor.systemGreen.withAlphaComponent(0.3)
         static let buttonTitleColor: UIColor = .darkGray
+      
     }
 
     // MARK: - Variables
@@ -58,28 +60,61 @@ final class SignUpViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupUI()
+        let backButton = UIButton(type: .system)
+            backButton.setImage(UIImage(systemName: "arrow.left"), for: .normal) // Use custom image
+            backButton.setTitle("", for: .normal) // Remove text
+            backButton.tintColor = .black // Change the color if needed
+            backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+
+            navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+        }
+
+        @objc private func backButtonTapped() {
+            navigationController?.popViewController(animated: true)
+
     }
 
     // MARK: - Private Functions
     private func setupUI() {
         configureTitleLabel()
-        configureTextField(firstNameTextField, placeholder: "First Name")
-        configureTextField(lastNameTextField, placeholder: "Last Name")
-        configureTextField(emailTextField, placeholder: "Email address")
-        configureTextField(passwordTextField, placeholder: "Password")
         configureSignUpButton()
+        
+        // Adjustments for centering the fields
+        view.addSubview(firstNameTextField)
+        view.addSubview(lastNameTextField)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+
+        let textFields = [firstNameTextField, lastNameTextField, emailTextField, passwordTextField]
+
+        for (index, textField) in textFields.enumerated() {
+            configureTextField(textField, placeholder: ["First Name", "Last Name", "Email address", "Password"][index])
+            textField.font = UIFont(name: Constants.fontName, size: 15)
+            
+            if index == 0 {
+                // Center the first text field vertically
+                textField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -(Constants.textFieldHeight + Constants.fieldSpacing) * CGFloat(textFields.count - 1) / 2).isActive = true
+                
+            } else {
+                // Position subsequent text fields below the previous ones
+                textField.topAnchor.constraint(equalTo: textFields[index - 1].bottomAnchor, constant: Constants.fieldSpacing).isActive = true
+            }
+        }
+
+        
     }
 
     private func configureTitleLabel() {
         view.addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.font = UIFont.boldSystemFont(ofSize: Constants.fontTitleSize)
+        titleLabel.font = UIFont(name: Constants.fontName, size: Constants.fontTitleSize)
         titleLabel.textAlignment = .left
         titleLabel.text = Constants.titleLabelText
         titleLabel.textColor = .black
+        
 
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 70),
             titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.sidePadding)
         ])
     }
@@ -91,22 +126,12 @@ final class SignUpViewController: UIViewController {
         textField.placeholder = placeholder
         textField.backgroundColor = UIColor.systemGray6
         textField.layer.cornerRadius = Constants.textFieldCornerRadius
-
-        let previousView: UIView = {
-            switch textField {
-            case firstNameTextField: return titleLabel
-            case lastNameTextField: return firstNameTextField
-            case emailTextField: return lastNameTextField
-            case passwordTextField: return emailTextField
-            default: return view
-            }
-        }()
+        textField.backgroundColor = Constants.textFieldColor
 
         NSLayoutConstraint.activate([
-            textField.topAnchor.constraint(equalTo: previousView.bottomAnchor, constant: Constants.fieldSpacing),
-            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.sidePadding),
-            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.sidePadding),
-            textField.heightAnchor.constraint(equalToConstant: Constants.textFieldHeight)
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor), // Center horizontally
+            textField.widthAnchor.constraint(equalTo: signUpButton.widthAnchor), // Match button width
+            textField.heightAnchor.constraint(equalToConstant: Constants.textFieldHeight) // Set fixed height
         ])
     }
 
@@ -114,6 +139,7 @@ final class SignUpViewController: UIViewController {
         view.addSubview(signUpButton)
         signUpButton.translatesAutoresizingMaskIntoConstraints = false
         signUpButton.setTitle("Sign Up", for: .normal)
+        signUpButton.titleLabel?.font = UIFont(name: Constants.fontName, size: 14)
         signUpButton.backgroundColor = Constants.buttonBackgroundColor
         signUpButton.setTitleColor(Constants.buttonTitleColor, for: .normal)
         signUpButton.layer.cornerRadius = Constants.buttonCornerRadius
