23a4234845846abfa68c2865cf5ae90ed89b07bd
vovaggri <vladimirgri0405@gmail.com>
2025-01-24 16:05:40 +0300

    Preparation for API

---
+++ WhatToDoPlanner/StartScreens/WelcomeScreen/WelcomeAPIClient.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeAPIClient.swift
+//  WhatToDoPlanner
+//
+//  Created by Vladimir Grigoryev on 24.01.2025.
+//
+
+import Foundation
+
+final class WelcomeAPIClient {
+//    static let shared = WelcomeAPIClient()
+//    private let baseURL = URL(string: "https:")
+}
