a2030a64009eda72f5e5bf1d747c70a7e7064902
vovaggri <vladimirgri0405@gmail.com>
2025-03-07 14:20:38 +0300

    Added label Today and Present button

--- WhatToDoPlanner/MainScreen/VC/BottomVC.swift
+++ WhatToDoPlanner/MainScreen/VC/BottomVC.swift
@@ -4,12 +4,57 @@
 import UIKit
 
 final class BottomSheetViewController: UIViewController {
+    enum Constants {
+        static let fontName: String = "AoboshiOne-Regular"
+        
+        static let textConstraints: Double = 20
+        static let textColor: String = "000000"
+        static let textSize: CGFloat = 20
+        
+        static let todayLabelText: String = "Todays tasks"
+        static let todayAlpha: CGFloat = 0.8
+        
+        static let switcherSmallText: String = "See all"
+        static let switcherLargreText: String = "Less"
+        static let switcherAlpha: CGFloat = 0.34
+    }
+    
+    private let todayLabel: UILabel = UILabel()
+    private let presentSwictherButton: UIButton = UIButton(type: .system)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(hex: "F0F1F1")
         
+        configureUI()
+    }
+    
+    private func configureUI() {
+        configureTodayLabel()
+        configureSwitcherButton()
+    }
+    
+    private func configureTodayLabel() {
+        view.addSubview(todayLabel)
         
+        todayLabel.font = UIFont(name: Constants.fontName, size: Constants.textSize)
+        todayLabel.text = Constants.todayLabelText
+        todayLabel.textColor = UIColor(hex: Constants.textColor, alpha: Constants.todayAlpha)
+        
+        todayLabel.translatesAutoresizingMaskIntoConstraints = false
+        todayLabel.pinTop(to: view, Constants.textConstraints)
+        todayLabel.pinLeft(to: view, Constants.textConstraints)
+    }
+    
+    private func configureSwitcherButton() {
+        view.addSubview(presentSwictherButton)
+        presentSwictherButton.setTitle(Constants.switcherSmallText, for: .normal)
+        presentSwictherButton.setTitleColor(UIColor(hex: Constants.textColor, alpha: Constants.switcherAlpha), for: .normal)
+        presentSwictherButton.titleLabel?.font = UIFont(name: Constants.fontName, size: Constants.textSize)
+        
+        presentSwictherButton.translatesAutoresizingMaskIntoConstraints = false
+        presentSwictherButton.pinTop(to: view, Constants.textConstraints)
+        presentSwictherButton.pinRight(to: view, Constants.textConstraints)
     }
 }
 
